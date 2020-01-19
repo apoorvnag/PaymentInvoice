73c7833077834763a2894c38e1402aa7c38e18e6
surajkanyal <59767099+surajkanyal@users.noreply.github.com>
2020-01-19 23:31:54 +0530

    updated maildata
    still bug

--- src/mail_jdbc/maildata.java
+++ src/mail_jdbc/maildata.java
@@ -25,7 +25,7 @@ public class maildata extends HttpServlet {
 		
 		Connection conn=null;
 		Statement stmt;
-		Statement stm;
+		Statement stm,st;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/g1","root","AmitKaushik$");
@@ -37,26 +37,39 @@ public class maildata extends HttpServlet {
 			
 			 Date cur_date= new java.util.Date();//current date
 			 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-			 sdf.format(cur_date);//formatting current date
-			while(rs0.next()) {			//running loop for users
+			 String a=sdf.format(cur_date);//formatting current date
+			
+			 
+			 while(rs0.next()) {			//running loop for users
 				
 				int u= rs0.getInt("id"); 
-				String user="select username form users where users.id='u'";//getting username for userid
-				String over="select due_date from invoices where invoices.user_id='u'"; //getting his all invoice duedate
+				st=conn.createStatement();
+
+				String vuser="select username from users where users.id='"+u+"'";
+				ResultSet rs1=st.executeQuery(vuser);
+				
+				String user= rs1.getString(1); //plzz check this line
+				
+				System.out.println(user);
+				String over="select due_date from invoices where invoices.user_id='"+u+"'"; //getting his all invoice duedate
+				
 				stm=conn.createStatement();
 				ResultSet rs2_1 = stm.executeQuery(over); //selecting due date 
 				
 				while(rs2_1.next()) {
+					
 					Date d=rs2_1.getDate("due_date"); //selecting each due date at a time
 					
-					sdf.format(d); //formatting date
-				    if(d.compareTo(cur_date)<0) { //comparing to current date
+					String b=sdf.format(d); //formatting date
+				
+					System.out.println(d);
+				    if(b.compareTo(a)<0) { //comparing to current date
 				    	
 				    	String sub="your invoice over its due date";
-				    	String cont=" you are requested to pay your invoice on time else interst wwill be charges accordingly.";
+				    	String cont=" you are requested to pay your invoice on time else interest will be charges accordingly.";
 				    	javamail.msend(user,sub,cont);
 				    	
-				    							}			
+				    							}		
 									}
 				rs2_1.close();
 			    			}
