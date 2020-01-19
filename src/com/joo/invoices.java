8391d3622afa6f236f06717ce72730fc5be175b2
Mansi <hastirmansi@gmail.com>
2020-01-19 15:59:55 +0530

    code done

---
+++ src/com/joo/invoices.java
@@ -0,0 +1,62 @@
+package com.joo;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+
+public class invoices extends HttpServlet
+
+{	private static final long serialVersionUID = 1L;
+/**
+ * @see HttpServlet#HttpServlet()
+ */
+public void InvoiceDashboard() {
+	InvoiceDashboard();
+    super();
+}
+public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+{
+   PrintWriter out = res.getWriter();
+   res.setContentType("text/html");
+   out.println("<html><body>");
+    	 try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/g","root","AmitKaushik$");
+	    	 Statement st = con.createStatement();
+	    	 ResultSet re = st.executeQuery("Select * from invoice");
+	    	 out.println("<table border=1 width=50% height=50%>");
+	           out.println("<tr><th>EmId</th><th>user</th><th>Status</th><tr>");
+	           while (re.next())
+	           {
+	               int n = re.getInt("id");
+	               int s = re.getInt("user_id");
+	               String nm = re.getString("Status");
+	               out.println("<tr><td>" + n + "</td><td>" + s + "</td><td>" + nm + "</td></tr>");
+	           }
+	           out.println("</table>");
+	           out.println("</html></body>");
+	           con.close();
+	           
+	    	 PrintWriter out = res.getWriter();
+	    	 re.next();
+	    	 out.println(re.getInt(1)+" "+ re.getInt(2)+ " "+ re.getString(3)+" "+ re.getString(4)
+	    	 +" "+re.getInt(5)+""+re.getInt(6));
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	 
+     } 
+}
